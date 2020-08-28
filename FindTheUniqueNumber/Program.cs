e8cca662efd105f40343d09eb3d495d0f9ff64f5
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-08-28 21:37:15 +0400

    Add FindTheUniqueNumber kata
    Add FindTheUniqueNumber kata and change target framework

---
+++ FindTheUniqueNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FindTheUniqueNumber
+{
+    class Program
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            //var my = numbers.GroupBy(x => x).Where(y => y.Count() == 1).Select(i => i.First()).ToArray().First();
+            return numbers.GroupBy(x => x).Single(x => x.Count() == 1).Key;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] test3 = new[] { 11, 11, 14, 11, 11 };
+
+            Console.WriteLine(GetUnique(test3));
+            Console.ReadKey();
+        }
+    }
+}
