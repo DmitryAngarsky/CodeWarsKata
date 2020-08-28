e5aedce757a8783e5ad71357e80433866c893ae5
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-08-28 14:10:31 +0400

    Add SquareEveryDigit kata

---
+++ SquareEveryDigit/SquareEveryDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareEveryDigit
+{
+    class Program
+    {
+        public static int SquareDigits(int n)
+        {
+            return int.Parse(string.Concat(n.ToString().Select(x => (int)char.GetNumericValue(x) * (int)char.GetNumericValue(x))));
+        }
+
+        static void Main(string[] args)
+        {
+            int digit = 9119;
+            Console.WriteLine(SquareDigits(digit));
+            Console.ReadKey();
+        }
+    }
+}
