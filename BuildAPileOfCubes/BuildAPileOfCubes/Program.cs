5797c5cd6d5f3ca5707337253c7368a7c9480bfb
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-08-29 18:22:39 +0400

    Add BuildAPileOfCubes kata

---
+++ BuildAPileOfCubes/BuildAPileOfCubes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BuildAPileOfCubes
+{
+    class Program
+    {
+        public static long findNb(long m)
+        {
+            long n = 0;
+            long total = 0;
+
+            while (total < m)
+            {
+                n += 1;
+                total += (n * n * n);
+            }
+
+            return total == m ? n : -1;
+        }
+
+        static void Main(string[] args)
+        {
+            long test = 1071225;
+            Console.WriteLine(findNb(test));
+            Console.ReadKey();
+        }
+    }
+}
