31c28becd87b833e679bf43d4e478ec5b572698b
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-08-31 09:41:30 +0400

    Add Dubstep kata

---
+++ Dubstep/Dubstep/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Dubstep
+{
+    class Program
+    {
+        public static string SongDecoder(string input)
+        {
+            //return Regex.Replace(input, "(WUB)+", " ").Trim();
+            return string.Concat(input.Replace("WUB", " ").Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x + " ")).TrimEnd();
+        }
+        static void Main(string[] args)
+        {
+            string test = "WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB";
+
+            Console.WriteLine(SongDecoder(test));
+            Console.ReadKey();
+        }
+    }
+}
