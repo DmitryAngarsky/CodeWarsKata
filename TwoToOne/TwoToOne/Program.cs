574a4771ff268661fa36ff994da05a7225aac8c6
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-01 16:39:24 +0400

    Add TwoToOne kata

---
+++ TwoToOne/TwoToOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TwoToOne
+{
+    class Program
+    {
+        public static string Longest(string s1, string s2)
+        {
+            //return string.Concat(string.Concat(s1, s2).Distinct().OrderBy(x => x));
+            return string.Concat((s1 + s2).Distinct().OrderBy(x => x));
+        }
+
+        static void Main(string[] args)
+        {
+            string str1 = "xyaabbbccccdefww";
+            string str2 = "xxxxyyyyabklmopq";
+
+            Console.WriteLine(Longest(str1, str2));
+            Console.ReadKey();
+        }
+    }
+}
