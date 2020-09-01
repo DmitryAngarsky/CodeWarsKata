9aba6bd5c2735b396c762c50b8b51fd78b345800
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-01 17:21:34 +0400

    Add UniqueInOrder kata

---
+++ UniqueInOrder/UniqueInOrder/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueInOrder
+{
+    class Program
+    {
+
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            T previous = default(T);
+            foreach (T current in iterable)
+            {
+                if (!current.Equals(previous))
+                {
+                    previous = current;
+                    yield return current;
+                }
+            }
+        }
+
+        //List<T> list = new List<T>();
+
+        //    foreach(T element in iterable)
+        //    {
+        //        if (!list.Contains(element))
+        //        {
+        //            list.Add(element);
+        //        } else if(list.LastIndexOf(element) != list.Count - 1) {
+        //            list.Add(element);
+        //        }
+        //    }
+
+        //    return list;
+        //}
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("2");
+            list.Add("2");
+            list.Add("3");
+            list.Add("3");
+            list.Add("3");
+
+            //var test = UniqueInOrder(test);
+
+            string test = "AAAABBBCCDAABBB";
+            var iter = UniqueInOrder(test);
+            foreach (var elem in iter)
+            {
+                Console.WriteLine(elem);
+            }
+            Console.ReadKey();
+        }
+    }
+}
