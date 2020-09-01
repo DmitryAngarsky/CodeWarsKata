3c74294d577fb650a161b57123f724b3b1a3a1da
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-01 17:24:07 +0400

    Add PerfectSquare kata

---
+++ PerfectSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PerfectSquare
+{
+    class Program
+    {
+        public static long FindNextSquare(long num)
+        {
+            double sqrt = Math.Sqrt(num);
+            return num % sqrt == 0 ? (long)((sqrt + 1) * (sqrt + 1)) : -1;
+        }
+        static void Main(string[] args)
+        {
+            long ex = 319225;
+            Console.WriteLine(FindNextSquare(ex));
+            Console.ReadKey();
+        }
+    }
+}
