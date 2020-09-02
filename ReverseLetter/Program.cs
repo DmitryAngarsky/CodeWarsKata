9b1807f502eba83796031e28bd3b711cdc959d6b
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-02 09:26:23 +0400

    Add ReverseLetter kata

---
+++ ReverseLetter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseLetter
+{
+    class Program
+    {
+        public static string ReverseLetter(string str)
+        {
+            //My solution
+            //return string.Concat(Regex.Replace(str, "[^a-zA-Z]", string.Empty).Reverse());
+            return string.Concat(str.Where(char.IsLetter).Reverse());
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "ultr53o?n";
+
+            Console.WriteLine(ReverseLetter(test));
+            Console.ReadKey();
+        }
+    }
+}
