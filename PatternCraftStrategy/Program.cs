ede5d130a17ac1cc46c6a3db483664e2895d88c0
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-03 09:21:24 +0400

    Add PatternCraftStrategy kata

---
+++ PatternCraftStrategy/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PatternCraftStrategy
+{
+    public interface IUnit
+    {
+        int Position { get; set; }
+        void Move();
+        IMoveBehavior MoveBehavior { get; set; }
+    }
+
+    public interface IMoveBehavior
+    {
+        void Move(IUnit unit);
+    }
+
+    public class Fly : IMoveBehavior
+    {
+        public void Move(IUnit unit)
+        {
+            unit.Position += 10;
+        }
+    }
+
+    public class Walk : IMoveBehavior
+    {
+        public void Move(IUnit unit)
+        {
+            unit.Position += 1;
+        }
+    }
+
+    public class Viking : IUnit
+    {
+        public Viking()
+        {
+            MoveBehavior = new Walk();
+        }
+
+        public IMoveBehavior MoveBehavior { get; set; }
+
+        public int Position { get; set; }
+
+        public void Move()
+        {
+            MoveBehavior.Move(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnit viking = new Viking();
+
+            viking.Move();
+            viking.Move();
+
+            viking.MoveBehavior = new Fly();
+            viking.Move();
+            Console.WriteLine(viking.Position);
+            Console.ReadKey();
+        }
+    }
+}
