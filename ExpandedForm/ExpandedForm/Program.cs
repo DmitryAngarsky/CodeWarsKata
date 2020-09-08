56ba344f5a8a2cc1d0a9d9dc073865ac405b4754
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-08 10:08:38 +0400

    Add ExpandedForm kata

---
+++ ExpandedForm/ExpandedForm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ExpandedForm
+{
+    class Program
+    {
+        public static string ExpandedForm(long num)
+        {
+            //First solution
+            //var expForm = string.Concat($"{num}".ToCharArray()
+            //                                .Select((x, i) => string.Concat(x + new string('0', $"{num}".Count() - 1 - i) + " + "))
+            //                                .Where(x => !x.StartsWith('0')));
+
+            //return string.Concat(expForm).Remove(expForm.Length - 3);
+
+            //Refactoring
+            var expForm = $"{num}".Select((x, i) => x + new string('0', $"{num}".Length - 1 - i))
+                                  .Where(x => !x.StartsWith('0'));
+
+            return string.Join(" + ", expForm);
+        }
+
+        static void Main(string[] args)
+        {
+            long test = 70302;
+
+            Console.WriteLine(ExpandedForm(test));
+            Console.ReadKey();
+        }
+    }
+    
+}
