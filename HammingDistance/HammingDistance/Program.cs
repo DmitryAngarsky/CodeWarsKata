c29a605c4f1588d60a5d19a5613c6bb1390bc52f
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-09 09:36:29 +0400

    Add HammingDistance kata

---
+++ HammingDistance/HammingDistance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace HammingDistance
+{
+    class Program
+    {
+        public static int Distance(string a, string b)
+        {
+            //return a.Zip(b, (x, y) => x != y).Count(x => x == true);
+            //return Enumerable.Range(0, a.Length).Count(i => a[i] != b[i]);
+            return a.Where((x, i) => a[i] != b[i]).Count();
+        }
+
+        static void Main(string[] args)
+        {
+            string str1 = "I like turtles";
+            string str2 = "I like turkeys";
+
+            Console.WriteLine(Distance(str1, str2));
+            Console.ReadKey();
+        }
+    }
+}
