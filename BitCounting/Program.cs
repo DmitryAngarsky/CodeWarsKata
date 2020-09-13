a46fc88bb235656a1a21b86a5ffa32616e25f7c5
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-13 11:24:32 +0400

    Add BitCounting kata

---
+++ BitCounting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace BitCounting
+{
+    class Program
+    {
+        public static int CountBits(int n)
+        {
+            //return (n == 0) ? 0 : (n & 1) + CountBits(n >> 1);
+            return Convert.ToString(n, 2).Count(c => c == '1');
+        }
+
+        static void Main(string[] args)
+        {
+            int test = 1234;
+
+            Console.WriteLine(CountBits(test));
+            Console.ReadKey();
+        }
+    }
+}
