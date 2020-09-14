a4e81f1e7a5320100b479b04322eb3ec1ddf771a
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-14 09:15:38 +0400

    Update DigitalRoot kata

--- DigitalRoot/DigitalRoot/Program.cs
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -4,9 +4,18 @@ namespace DigitalRoot
 {
     class Program
     {
+        public static int DigitalRoot(long n)
+        {
+            return (int)(1 + (n - 1) % 9);
+            //return n < 10 ? (int)n : DigitalRoot((long)n.ToString().Select(x => char.GetNumericValue(x)).Sum());
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long test = 493193;
+
+            Console.WriteLine(DigitalRoot(test));
+            Console.ReadKey();
         }
     }
 }
