2a31f04b9d7e2ce7931efa77b9abc7d79a6c46af
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-15 09:16:51 +0400

    Add NumberFormat kata

---
+++ NumberFormat/NumberFormat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumberFormat
+{
+    class Program
+    {
+        public static string NumberFormat(int number)
+        {
+            return number.ToString("N0", CultureInfo.InvariantCulture);
+            //return $"{number:N0}";
+        }
+
+        static void Main(string[] args)
+        {
+            int test = -420902;
+
+            Console.WriteLine(NumberFormat(test));
+            Console.ReadKey();
+        }
+    }
+}
