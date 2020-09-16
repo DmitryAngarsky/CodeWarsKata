17e3034d7541d8889c28f9a31ce4a0a456786459
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-16 09:47:27 +0400

    Update StringDuplicates kata

--- StringDuplicates/StringDuplicates/Program.cs
+++ StringDuplicates/StringDuplicates/Program.cs
@@ -6,30 +6,37 @@ namespace StringDuplicates
 {
     class Program
     {
-        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
-        {
-            T previous = default(T);
-            foreach (T current in iterable)
-            {
-                if (!current.Equals(previous))
-                {
-                    previous = current;
-                    yield return current;
-                }
-            }
-        }
-
-        public static string[] dup(string[] arr)
-            => arr.Select(x => x.Aggregate("", (c, n) => c.EndsWith(n) ? c : c + n)).ToArray();
+        // My solution
+        //public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        //{
+        //    T previous = default(T);
+        //    foreach (T current in iterable)
+        //    {
+        //        if (!current.Equals(previous))
+        //        {
+        //            previous = current;
+        //            yield return current;
+        //        }
+        //    }
+        //}
 
         //public static string[] dup(string[] arr)
         //{
         //    return arr.Select(x => string.Concat(UniqueInOrder(x))).ToArray();
         //}
 
+        public static string[] dup(string[] arr)
+            => arr.Select(x => x.Aggregate("", (c, n) => c.EndsWith(n) ? c : c + n)).ToArray();
+
+        //Regex
+        //public static string[] dup(string[] arr)
+        //{
+        //    return arr.Select(x => Regex.Replace(x, "(.)\\1+", "$1")).ToArray();
+        //}
+
         static void Main(string[] args)
         {
-            string[] test = new String[] { "abracadabra","allottee","assessee" };
+            string[] test = new string[] { "abracadabra","allottee","assessee" };
 
             foreach(string s in dup(test))
             {
