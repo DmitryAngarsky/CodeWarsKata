64b819fac5f5348537f7e4a81eb47eadd9bf70e9
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-17 09:38:59 +0400

    Add CountSmileys kata

---
+++ CountSmileys/CountSmileys/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CountSmileys
+{
+    class Program
+    {
+        public static int CountSmileys(string[] smileys)
+        {
+            //Regex.IsMatch(s, @"^[:;]{1}[~-]{0,1}[\)D]{1}$")
+            // Regex.Matches(string.Join(" ",smileys), "([:;][-~]?)[)D]")
+            return smileys.Where(x => Regex.IsMatch(x, @"^[;:][-~]?[D)]$")).Count();
+        }
+
+        static void Main(string[] args)
+        {
+            string[] test = new string[] { ";", ")", ";*", ":$", "8-D" };
+
+            Console.WriteLine(CountSmileys(test));
+            Console.ReadKey();
+        }
+    }
+}
