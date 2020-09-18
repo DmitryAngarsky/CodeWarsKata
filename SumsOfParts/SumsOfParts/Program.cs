87f413acf1f93d01008bf5b6c0c2395775fbd9e8
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-18 11:13:08 +0400

    Add SumsOfParts kata

---
+++ SumsOfParts/SumsOfParts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumsOfParts
+{
+    class Program
+    {
+        //public IEnumerable<int> GetSum(IEnumerable<int> iterable, int n)
+        //{
+        //    int result = 0;
+        //    for(int i = 0; i < iterable.LongCount() - n; i++)
+        //    {
+        //        yield return result += iterable[i];
+        //    }
+        //}
+        public static int[] PartsSums(int[] ls)
+        {
+            return Enumerable.Range(0, ls.Length)
+                             .Select(n => ls.Skip(n).Sum()).Append(0).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var test = new int[] { 0, 1, 3, 6, 10 };
+
+            foreach(int n in PartsSums(test))
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
