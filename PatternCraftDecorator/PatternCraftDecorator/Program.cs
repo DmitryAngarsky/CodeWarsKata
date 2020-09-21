e9de9646c4efb7f98eafbc1539168c3534cde97e
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-21 14:27:38 +0400

    Add PatternCraftDecorator kata

---
+++ PatternCraftDecorator/PatternCraftDecorator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace PatternCraftDecorator
+{
+    public interface IMarine
+    {
+        int Damage { get; set; }
+        int Armor { get; set; }
+    }
+
+    public class Marine : IMarine
+    {
+        public Marine(int damage, int armor)
+        {
+            Damage = damage;
+            Armor = armor;
+        }
+
+        public int Damage { get; set; }
+        public int Armor { get; set; }
+    }
+
+    public class MarinreDecorator : IMarine
+    {
+        IMarine marine;
+
+        public MarinreDecorator(IMarine marine)
+        {
+            this.marine = marine;
+        }
+
+        public virtual int Damage { get => marine.Damage; set => marine.Damage = value; }
+        public virtual int Armor { get => marine.Armor; set => marine.Armor = value; }
+    }
+
+    public class MarineWeaponUpgrade : MarinreDecorator
+    {
+        private IMarine marine;
+
+        public MarineWeaponUpgrade(IMarine marine) : base(marine)
+        {
+            this.marine = marine;
+        }
+
+        public override int Damage => marine.Damage + 1;
+    }
+
+    public class MarineArmorUpgrade : MarinreDecorator 
+    {
+        private IMarine marine;
+
+        public MarineArmorUpgrade(IMarine marine) : base(marine)
+        {
+            this.marine = marine;
+        }
+
+        public override int Armor => marine.Armor + 1;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMarine marine = new Marine(10, 1);
+
+            marine = new MarineWeaponUpgrade(marine);
+            marine = new MarineWeaponUpgrade(marine);
+
+            marine = new MarineArmorUpgrade(marine);
+            marine = new MarineArmorUpgrade(marine);
+
+            Console.WriteLine(marine.Damage);
+            Console.WriteLine(marine.Armor);
+            Console.ReadKey();
+        }
+    }
+}
--- SumsOfParts/SumsOfParts/Program.cs
+++ SumsOfParts/SumsOfParts/Program.cs
@@ -6,25 +6,35 @@ namespace SumsOfParts
 {
     class Program
     {
-        //public IEnumerable<int> GetSum(IEnumerable<int> iterable, int n)
-        //{
-        //    int result = 0;
-        //    for(int i = 0; i < iterable.LongCount() - n; i++)
-        //    {
-        //        yield return result += iterable[i];
-        //    }
-        //}
+        public static IEnumerable<int> GetSum(IEnumerable<int> iterable)
+        {
+            int result = 0;
+            foreach(var n in iterable)
+            {
+                yield return result += n;
+            }
+        }
+        //return Enumerable.Range(0, ls.Length)
+        //                     .Select(n => ls.Skip(n).Sum()).Append(0).ToArray();
+        
         public static int[] PartsSums(int[] ls)
         {
-            return Enumerable.Range(0, ls.Length)
-                             .Select(n => ls.Skip(n).Sum()).Append(0).ToArray();
+            int[] result = new int[ls.Length + 1];
+            result[ls.Length] = 0;
+            
+            for (int i = 0; i < ls.Length; i++)
+            {
+                result[i] = ls.Skip(i).Sum();
+            }
+
+            return result;
         }
 
         static void Main(string[] args)
         {
             var test = new int[] { 0, 1, 3, 6, 10 };
-
-            foreach(int n in PartsSums(test))
+            
+            foreach(int n in GetSum(test))
             {
                 Console.WriteLine(n);
             }
