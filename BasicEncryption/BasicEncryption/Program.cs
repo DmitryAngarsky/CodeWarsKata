f44f7682a74fb13844d486956d24065c2e4f5e36
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-23 16:37:46 +0400

    Add BasicEncryption kata

---
+++ BasicEncryption/BasicEncryption/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BasicEncryption
+{
+    class Program
+    {
+        public static string Encrypt(string text, int rule)
+        {
+            var cw = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(text).Select(b => (byte)(b + rule )).ToArray());
+            var test = Encoding.ASCII.GetString(text.Select(a => (byte)(a + rule & 255)).ToArray());
+            var test1 = text.Aggregate("", (a, b) => Encoding.ASCII.GetString(new byte[] { (byte)b }));
+
+            return test1;
+        }
+
+        static void Main(string[] args)
+        {
+            string test = Encrypt("please encrypt me6", 2);
+
+            Console.WriteLine(test);
+            Console.ReadKey();
+        }
+    }
+}
