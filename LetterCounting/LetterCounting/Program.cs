75fca9ab005e637997b581e43925a64e82b0df76
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-25 09:45:40 +0400

    Add LetterCounting kata

---
+++ LetterCounting/LetterCounting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace LetterCounting
+{
+    class Program
+    {
+        // Best solution
+        //public static string StringLetterCount(string str)
+        //    => string.Concat(
+        //        str
+        //          .Where(char.IsLetter)
+        //          .GroupBy(char.ToLowerInvariant)
+        //          .OrderBy(g => g.Key)
+        //          .Select(g => $"{g.Count()}{g.Key}"));
+
+        public static string StringLetterCount(string str)
+        {
+            string lowerStr = str.Replace(" ", "").ToLower();
+            return string.Concat(
+                lowerStr
+                .Where(ch => char.IsLetter(ch))
+                .Select(c => $"{lowerStr.Count(s => s == c)}" + c)
+                .Distinct()
+                .OrderBy(x => x[1])
+            );
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "This is a test sentence.";
+
+            Console.WriteLine(StringLetterCount(test));
+            Console.ReadKey();
+        }
+    }
+}
