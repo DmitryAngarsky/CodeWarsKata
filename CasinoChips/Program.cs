bb3342bb682143e2e06ced610cf2b108e7b16040
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-27 13:12:04 +0400

    Add CasinoChips kata

---
+++ CasinoChips/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CasinoChips
+{
+    class Program
+    {
+        public static int solve(int[] arr)
+        {
+            int result = 0;
+            Array.Sort(arr);
+            while (arr[1] > 0)
+            {
+                arr[2]--;
+                arr[1]--;
+                result++;
+                Array.Sort(arr);
+            }
+
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] test = { 7, 4, 10 };
+            
+            Console.WriteLine(solve(test));
+            Console.ReadKey();
+        }
+    }
+}
