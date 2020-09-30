f5dec3639922e9862122f92785c7594057efd577
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-30 10:19:36 +0400

    Add RGBToHexConversion kata

---
+++ RGBToHexConversion/RGBToHexConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace RGBToHexConversion
+{
+    class Program
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            return string.Concat(
+                new int[] { r, g, b }
+                .Select(i => Math.Clamp(i, 0 , 255).ToString("X2")));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rgb(211, 53, 12));
+            Console.ReadKey();
+        }
+    }
+}
