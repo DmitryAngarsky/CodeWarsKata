30c9f56e137ea239a596a34607f13121d29eb5cc
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-05 09:23:06 +0400

    Add TriangleNumber kata

---
+++ TriangleNumber/TriangleNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TriangleNumber
+{
+    class Program
+    {
+        public static bool isTriangleNumber(long number)
+        {
+            return Math.Sqrt(8 * number + 1) % 1 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            long test = 125250;
+
+            Console.WriteLine(isTriangleNumber(test));
+            Console.ReadKey();
+        }
+    }
+}
