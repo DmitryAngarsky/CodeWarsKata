433e33328ce0433fbb098607f5e99bc47fd9edd9
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-06 17:51:41 +0400

    Add Base64 kata

---
+++ Base64Encoding/Base64Encoding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Base64Encoding
+{
+    class Program
+    {
+        public static string ToBase64(string s)
+        {
+            var test = s.Select(c => Convert.ToString(c, 2));
+            foreach(string b in test)
+            {
+                Console.WriteLine(b);
+            }
+            return s;
+        }
+
+        public static string FromBase64(string s)
+        {
+            // Happy coding!
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "this is a string!!";
+            string test1 = "dGhpcyBpcyBhIHN0cmluZyEh";
+
+            Console.WriteLine(ToBase64(test));
+            Console.WriteLine(FromBase64(test1));
+            Console.ReadKey();
+        }
+    }
+}
