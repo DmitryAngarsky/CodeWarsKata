8d965f20cb32fa9308d16cce9c0cd4bfbeab5dc2
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-09 09:50:52 +0400

    Add WordsToHex kata

---
+++ WordsToHex/WordsToHex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsToHex
+{
+    class Program
+    {
+        public static string[] WordsToHex(string words)
+        {
+            string ToHex(string str) => "#" + string.Join("", str.Take(3).Select(c => Convert.ToString(c, 16))).PadRight(6, '0');
+            return words
+                .Split(" ")
+                .Select(w => ToHex(w))
+                .ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "Hello, my name is Gary and I like cheese.";
+
+            Console.WriteLine(WordsToHex(test));
+            Console.ReadKey();
+        }
+    }
+}
