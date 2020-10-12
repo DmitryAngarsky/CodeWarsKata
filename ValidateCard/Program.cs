6bc57a1eb25ad0733d2f30c6ab0de132644dcc7a
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-12 21:34:18 +0400

    Add ValidateCard kata

---
+++ ValidateCard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidateCard
+{
+    internal static class Program
+    {
+        private static bool Validate(string n)
+        {
+            return n
+                .Replace(" ", "")
+                .Reverse()
+                .Select((num, i) => i % 2 != 0 ? (num - '0') * 2 : num - '0')
+                .Select(s => s > 9 ?  s - 9 : s)
+                .Sum() % 10 == 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            string test1 = "477 073 360";
+            const string test2 = "5422 0148 5514";
+            
+            Console.WriteLine(Validate(test1));
+            Console.ReadKey();
+        }
+    }
+}
