b42c2e25856a1e5167103db363711aa2456781ea
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-15 09:55:36 +0400

    Add  Palindromes

---
+++ Palindromes/Palindromes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindromes
+{
+    class Program
+    {
+        public static string solve(string s)
+        {
+            bool isPolindrom(string str) => str == string.Concat(str.Reverse());
+
+            if (isPolindrom(s))
+            {
+                return "Ok";
+            }
+            else
+            {
+                return s.Select((c, i) => isPolindrom(s.Remove(i, 1))).Contains(true) ? "remove one" : "not possible"; ;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "madmam";
+
+            Console.WriteLine(solve(test));
+            Console.ReadKey();
+        }
+    }
+}
