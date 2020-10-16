f529296761b1ab1c5c6bfe017cc255a18fcb2ee2
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-16 09:28:58 +0400

    Add ZooPatrol

---
+++ ZooPatrol/ZooPatrol/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ZooPatrol
+{
+    class Program
+    {
+        public static int FindNumber(int[] array)
+        {
+            var test = Enumerable.Range(1, array.Length + 1).Except(array);
+            return test.First();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 13, 11, 10, 3, 2, 1, 4, 5, 6, 9, 7, 8 };
+
+            Console.WriteLine(FindNumber(test));
+            Console.ReadKey();
+        }
+    }
+}
