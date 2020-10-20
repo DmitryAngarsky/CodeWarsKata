b7cdc5b42e3c677cf06e9f771afc4be939146e50
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-20 09:31:12 +0400

    Add CutRopes

---
+++ CutRopes/CutRopes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CutRopes
+{
+    class Program
+    {
+        public static int[] CutTheRopes(int[] a)
+        {
+            List<int> sourceArray = new List<int>(a);
+            List<int> result = new List<int>();
+
+            while (sourceArray.Count != 0)
+            {
+                result.Add(sourceArray.Count());
+                int min = sourceArray.Min();
+
+                sourceArray = sourceArray.Select(x => x - min).Where(f => f > 0).ToList();
+            }
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 3, 3, 2, 9, 7 };
+
+            foreach(int a in CutTheRopes(test))
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
