92d581f241b5f48e38c75b0fa9fcd6a4196bcea6
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-24 18:46:37 +0400

    Add RemoveParentheses

---
+++ RemoveParentheses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RemoveParentheses
+{
+    class Program
+    {
+        public static string RemoveParentheses(string s)
+        {
+            var str = s;
+            var result = new Regex(@"\(.*\)").Replace(s, "");
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            const string test = "(first group) (second group) (third group)";
+            
+            Console.WriteLine(RemoveParentheses(test));
+            Console.ReadKey();
+        }
+    }
+}
