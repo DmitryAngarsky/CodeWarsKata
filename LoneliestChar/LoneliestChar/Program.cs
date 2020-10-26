5998978f4c6e36af1f20abb382552458dc2d31d9
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-26 09:47:37 +0400

    Add Visitor

---
+++ LoneliestChar/LoneliestChar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LoneliestChar
+{
+    class Program
+    {
+        public static char[] Loneliest(string result)
+        {
+            return result.ToCharArray();
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "    a b  sc     p     t   k";
+
+            Console.WriteLine(Loneliest(test));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Visitor/Visitor/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Visitor
+{
+    public interface IVisitor
+    {
+        void VisitLight(ILightUnit unit);
+        void VisitArmored(IArmoredUnit unit);
+    }
+
+    public interface ILightUnit
+    {
+        int Health { get; set; }
+
+        void Accept(IVisitor visitor);
+    }
+
+    public interface IArmoredUnit
+    {
+        int Health { get; set; }
+
+        void Accept(IVisitor visitor);
+    }
+
+    public class Marine : ILightUnit
+    {
+        public int Health { get; set; } = 100;
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitLight(this);
+        }
+    }
+
+    public class Marauder : IArmoredUnit
+    {
+        public int Health { get; set; } = 125;
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitArmored(this);
+        }
+    }
+
+    public class TankBullet : IVisitor
+    {
+        public void VisitLight(ILightUnit unit)
+        {
+            unit.Health -= 21;
+        }
+
+        public void VisitArmored(IArmoredUnit unit)
+        {
+            unit.Health -= 32;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVisitor bullet = new TankBullet();
+            ILightUnit light = new Marine();
+
+            light.Accept(bullet);
+
+            Console.WriteLine($"Marine health - {light.Health}");
+
+            IVisitor tankBullet = new TankBullet();
+            IArmoredUnit armored = new Marauder();
+
+            armored.Accept(bullet);
+
+            Console.WriteLine($"Marauder health - {armored.Health}");
+
+            Console.ReadKey();
+        }
+    }
+}
