8b8105fd433b1d05974382e5c3947c5cdc24ac07
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-29 12:48:57 +0400

    Add AlphabetPos

---
+++ AlphabetPosition/AlphabetPosition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AlphabetPosition
+{
+    class Program
+    {
+        public static string AlphabetPosition(string text)
+        {
+            //.Select(c => "abcdefghijklmnopqrstuvwxyz".IndexOf(c) + 1)
+            //return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(x => x - 'a' + 1));
+            return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(c => c - 96));
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "The sunset sets at twelve o' clock.";
+
+            Console.WriteLine(AlphabetPosition(test));
+            Console.ReadKey();
+        }
+    }
+}
