a3f8e4913d25d4ca0112a28f166e768ae409fcc1
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-30 09:29:22 +0400

    Add DigitalCypher_2

---
+++ DigitalCypher_2/DigitalCypher_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace DigitalCypher_2
+{
+    class Program
+    {
+        public static string Decode(int[] code, int key)
+        {
+            return string.Join("", code.Select((c, i) => (char)(96 + c - ($"{key}"[i % $"{key}".Length] - '0'))));
+        }
+
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 20, 12, 18, 30, 21 };
+
+            Console.WriteLine(Decode(test, 1939));
+            Console.ReadKey();
+        }
+    }
+}
