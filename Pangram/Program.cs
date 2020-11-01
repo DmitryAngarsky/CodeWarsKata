816c8628b2ee73f4e2f7d1367c99ada35405fe84
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-01 17:32:51 +0400

    Add Pangram

---
+++ Pangram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Pangram
+{
+    class Program
+    {
+        // public static bool IsPangram(string str) =>
+        //     str.ToLower()
+        //         .Where(Char.IsLetter)
+        //         .Distinct()
+        //         .Count() == 26;
+
+        private static bool IsPangram(string str)
+        {
+            return !Enumerable.Range(1, 26).Except(str.ToLower()
+                .Where(char.IsLetter)
+                .Select(c => c - 96))
+                .Any();
+        }
+        
+        static void Main(string[] args)
+        {
+            const string test = "The quick brown fox jumps over the lazy dog.";
+            
+            Console.WriteLine(IsPangram(test));
+            Console.ReadKey();
+        }
+    }
+}
