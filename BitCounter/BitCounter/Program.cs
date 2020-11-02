843098833ba8d064fff083456fa007bad4a83e51
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-02 09:28:25 +0400

    Add BitCounter

---
+++ BitCounter/BitCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BitCounter
+{
+    class Program
+    {
+        public static bool SharedBits(int a, int b)
+        {
+            return Convert.ToString(a & b, 2).Replace("0", "").Length >= 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SharedBits(10, 15));
+            Console.ReadKey();
+        }
+    }
+}
