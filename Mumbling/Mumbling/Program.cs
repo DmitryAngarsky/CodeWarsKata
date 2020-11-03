bc265cb7542ba612094a0b18f30def98a4fba1c0
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-03 17:55:27 +0400

    Add Mumbling

---
+++ Mumbling/Mumbling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Mumbling
+{
+    class Program
+    {
+        public static string Accum(string s)
+        {
+            //return string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)));
+            return string.Join("-", s.Select((c, i) => char.ToUpper(c).ToString() + string.Concat(Enumerable.Repeat(char.ToLower(c), i)))); ;
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "abcd";
+
+            Console.WriteLine(Accum(test));
+            Console.ReadKey();
+        }
+    }
+}
