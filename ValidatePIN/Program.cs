cbd51ae43cf5be751c522156fbcc432029a6ec25
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-04 18:17:05 +0400

    Add ValidatePIN

---
+++ ValidatePIN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ValidatePIN
+{
+    class Program
+    {
+        public static bool ValidatePin(string pin)
+        {
+            return (pin.Length == 4
+                   || pin.Length == 6)
+                   && pin.All(char.IsDigit);
+        }
+        
+        static void Main(string[] args)
+        {
+            string test = "a234";
+            
+            Console.WriteLine(ValidatePin(test));
+            Console.ReadKey();
+        }
+    }
+}
