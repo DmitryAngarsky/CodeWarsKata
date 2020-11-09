aa01408ab6e6b9abf6051ea3b942006674888bb6
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-09 09:29:27 +0400

    Add BinaryAddition

---
+++ BinaryAddition/BinaryAddition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryAddition
+{
+    class Program
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString(a + b, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary(1, 2));
+            Console.ReadKey();
+        }
+    }
+}
