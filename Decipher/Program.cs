34472d38c71fe51279281256b75ede24a68855ce
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-10 19:30:20 +0400

    Add Decipher

---
+++ Decipher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Decipher
+{
+    class Program
+    {
+        private static string DecipherThis(string s)
+        {
+            string ReplaceChar(string str) => str.Length >= 2 
+                ? str[^1] + str.Substring(1, str.Length - 2) + str[0]
+                : str;
+
+            return string.IsNullOrWhiteSpace(s) 
+                ? string.Empty 
+                : string.Join(" ", s
+                    .Split(" ")
+                    .Select(w => $"{(char)int.Parse(string.Concat(w.TakeWhile(char.IsDigit)))}" 
+                                            + ReplaceChar(string.Concat(w.SkipWhile(char.IsDigit)))));
+        }
+        
+        static void Main(string[] args)
+        {
+            string test = " ";
+            
+            Console.WriteLine(DecipherThis(test));
+            Console.ReadKey();
+        }
+    }
+}
