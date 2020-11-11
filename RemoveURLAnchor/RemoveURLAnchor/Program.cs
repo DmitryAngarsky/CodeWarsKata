978f184b9f2de16e5b5b9296b03533173f834edf
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-11 09:14:37 +0400

    Add RemoveUrlAnchor

---
+++ RemoveURLAnchor/RemoveURLAnchor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveURLAnchor
+{
+    class Program
+    {
+        public static string RemoveUrlAnchor(string url)
+        {
+            var test = url.TakeWhile(c => c != '#');
+            return string.Concat(url.TakeWhile(c => c != '#'));
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "www.codewars.com/katas/?page=1#about";
+
+            Console.WriteLine(RemoveUrlAnchor(test));
+            Console.ReadKey();
+        }
+    }
+}
