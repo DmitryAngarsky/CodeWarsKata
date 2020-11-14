6c3b593bb14699c5f568ded391ac6805fe60b8c2
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-14 07:03:12 +0400

    Add PlayingWithDigits

---
+++ PlayingWithDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PlayingWithDigits
+{
+    class Program
+    {
+        private static long DigPow(int n, int p)
+        {
+            long magic = (long)$"{n}"
+                .Select((c, i) => Math.Pow(c - '0', p + i))
+                .Sum();
+            
+            return magic % n == 0 ? magic / n : -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigPow(46288, 3));
+            Console.ReadKey();
+        }
+    }
+}
