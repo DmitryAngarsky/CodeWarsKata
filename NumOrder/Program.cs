6e50db578f2377e8e4907265b7531b16b26fc6c3
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-15 12:09:00 +0400

    Add NumOrder

---
+++ NumOrder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace NumOrder
+{
+    class Program
+    {
+        private static string Order(string words)
+        {
+            return string.IsNullOrEmpty(words)
+                ? string.Empty
+                : string.Join(" ", words.Split().OrderBy(w => w.SingleOrDefault(char.IsDigit)));
+            
+            // return string.Join(" ", words.Split().OrderBy(w => (int)w.FirstOrDefault(char.IsDigit)));
+        }
+          
+        static void Main(string[] args)
+        {
+            const string theory = "Thi1s is2 3a T4est";
+            const string fact = "is2 Thi1s T4est 3a";
+            
+            Console.WriteLine(theory.Equals(Order(fact)));
+            Console.ReadKey();
+        }
+    }
+}
