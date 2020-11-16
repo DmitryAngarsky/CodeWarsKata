829a20d0fadfd68122cc9c7b44e99362e60d3b4b
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-16 09:41:36 +0400

    Add BreakСamelCase

---
+++ BreakСamelCase/BreakСamelCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace BreakСamelCase
+{
+    class Program
+    {
+        public static string BreakCamelCase(string str)
+        {
+            return string.Concat(str.Select(c => char.IsUpper(c) ? " " + c : $"{c}"));
+        }
+
+        static void Main(string[] args)
+        {
+            string test = "camelCasing";
+
+            Console.WriteLine(BreakCamelCase(test));
+            Console.ReadKey();
+        }
+    }
+}
