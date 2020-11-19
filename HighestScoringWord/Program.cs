74dbf72c0b2c80565258dfc1abca8bd56f00086e
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-19 19:34:30 +0400

    Add HighestScoringWord

---
+++ HighestScoringWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace HighestScoringWord
+{
+    class Program
+    {
+        private static string High(string s)
+        {
+            return s.Split().OrderBy(w => w.Sum(c => c - 96)).Last();
+            return s.Split()
+                .Select(w => $"{w.Sum(c => c - 96)} " + w)
+                .OrderByDescending(a => int.Parse(a.Split()[0]))
+                .First().Split()[1];
+        }
+        
+        static void Main()
+        {
+            string test = "man i need a taxi up to ubud";
+            
+            Console.WriteLine(High(test));
+            Console.ReadKey();
+        }
+    }
+}
