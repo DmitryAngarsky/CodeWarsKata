5759c9fb19314bb0a84cd24237bb2277ca0e68f0
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-20 21:05:16 +0400

    Add MinimizeSumOfArray

---
+++ MinimizeSumOfArray/MinimizeSumOfArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimizeSumOfArray
+{
+    class Program
+    {
+        private static int MinSum(int[] a)
+        {
+            return a.OrderBy(n => n)
+                .Skip(a.Length / 2)
+                .Zip(a.OrderByDescending(n => n)
+                    .Skip(a.Length / 2), (first, second) => first * second)
+                .Sum();
+            
+            IOrderedEnumerable<int> orderedArray = a.OrderBy(i => i);
+
+            return orderedArray.Take(a.Length / 2)
+                .Zip(orderedArray.Skip(a.Length / 2).Reverse(), (first, second) => first * second)
+                .Sum();
+        }
+        
+        static void Main()
+        {
+            int[] test = {9,2,8,7,5,4,0,6};
+            
+            Console.WriteLine(MinSum(test));
+            Console.ReadKey();
+        }
+    }
+}
