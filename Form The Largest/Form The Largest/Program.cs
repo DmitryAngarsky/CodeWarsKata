411eb3723b1e2c83cc1dcea8f9abc9062bff5571
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-21 23:07:03 +0400

    Add Form_The_Largest

---
+++ Form The Largest/Form The Largest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Form_The_Largest
+{
+    class Program
+    {
+        private static int MaxNumber(int n)
+        {
+            return int.Parse($"{n}".OrderByDescending(c => c - '0').ToArray());
+            return int.Parse(string.Concat($"{n}".OrderByDescending(c => c - '0')));
+        }
+        
+        static void Main(string[] args)
+        {
+            int test = 566797;
+            
+            Console.WriteLine(MaxNumber(test));
+            Console.ReadKey();
+        }
+    }
+}
