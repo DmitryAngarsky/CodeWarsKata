6f8ccfc2648b729b24ac9e9a9a3bc13f244f1f30
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-22 23:49:17 +0400

    Add AlphabetWar

---
+++ AlphabetWar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlphabetWar
+{
+    class Program
+    {
+        public static Dictionary<char, int> LeftSide = new Dictionary<char, int>()
+        {
+            {'w', 4},
+            {'p', 3},
+            {'b', 2},
+            {'s', 1}
+        };
+        
+        public static Dictionary<char, int> RightSide = new Dictionary<char, int>()
+        {
+            {'m', 4},
+            {'q', 3},
+            {'d', 2},
+            {'z', 1}
+        };
+        
+        public static string AlphabetWar(string fight)
+        {
+            var test = fight.Aggregate(0, (c, i) => LeftSide.ContainsKey(i) ? -LeftSide[i] : RightSide[i]);
+            return test == 0 ? "Let's fight again!" : test > 0 ? "Right side wins!" : "Left side wins!";
+        }
+        
+        static void Main(string[] args)
+        {
+            string test = "zdqmwpbs";
+            
+            Console.WriteLine(AlphabetWar(test));
+            Console.ReadKey();
+        }
+    }
+}
