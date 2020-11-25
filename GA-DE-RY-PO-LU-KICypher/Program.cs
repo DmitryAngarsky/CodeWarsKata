26aba5ab1411685ca3a7f24bae0946e5bd631b78
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-25 21:54:05 +0400

    Add GA_DE_RY_PO_LU_KICypher decode

--- GA-DE-RY-PO-LU-KICypher/Program.cs
+++ GA-DE-RY-PO-LU-KICypher/Program.cs
@@ -5,11 +5,19 @@ namespace GA_DE_RY_PO_LU_KICypher
 {
     class Program
     {
-        public static string Encode(string str)
+        public static string Encode1(string str) => Decode1(str);
+        public static string Decode1(string str)
         {
-            string key = "GADERYPOLUKI";
+            var s = "GDRPLKgdrplkAEYOUIaeyoui";
+            var t = "AEYOUIaeyouiGDRPLKgdrplk";
+            return string.Concat(str.Select(x => s.IndexOf(x) < 0 ? x : t[s.IndexOf(x)]));
+        }
+        
+        private static string Translate(string str)
+        {
+            const string key = "GADERYPOLUKI";
 
-            var test = str.ToUpper()
+            char[] charsWithKey = str.ToUpper()
                 .Select(c => 
                     key.Contains(c) 
                         ? key.IndexOf(c) % 2 == 0 
@@ -21,20 +29,24 @@ namespace GA_DE_RY_PO_LU_KICypher
             return string.Concat(
                 str.Select((c, i) =>
                     char.IsUpper(c)
-                        ? char.ToUpper(test[i])
-                        : char.ToLower(test[i])));
+                        ? char.ToUpper(charsWithKey[i])
+                        : char.ToLower(charsWithKey[i])));
+        }
+        public static string Encode(string str)
+        {
+            return Translate(str);
         }
 
         public static string Decode(string str)
         {
-            return str;
+            return Translate(str);
         }
         
         static void Main(string[] args)
         {
-            string test = "gaderypoluki";
-            
-            Console.WriteLine("agedyropulik" == Encode(test));
+            string test = "agedyropulik";
+            Console.WriteLine(Decode(test));
+            Console.WriteLine("gaderypoluki" == Decode(test));
             Console.ReadKey();
         }
     }
