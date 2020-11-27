abc7322dd0868358e435ec705125ec1eba837d42
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-27 22:44:39 +0400

    Add NthPowerRules

---
+++ NthPowerRules/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace NthPowerRules
+{
+    class Program
+    {
+        public static int ModifiedSum(int[] a, int n)
+        {
+            return a.Sum(x => (int) Math.Pow(x, n) - x);
+            return a.Select(i => (int)Math.Pow(i, n)).Sum() - a.Sum();
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] test = {1, 2, 3};
+            
+            Console.WriteLine(ModifiedSum(test, 3));
+            Console.ReadKey();
+        }
+    }
+}
