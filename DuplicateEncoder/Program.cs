339b5a1e831485afaf61eb38ff1206245da8abde
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-29 22:51:31 +0400

    Add DuplicateEncoder

---
+++ DuplicateEncoder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace DuplicateEncoder
+{
+    class Program
+    {
+        public static string DuplicateEncode(string word)
+        {
+            return string.Concat(word.ToLower().Select(c => word.ToLower().Count(i => i == c) > 1 ? ')' : '('));
+        }
+        
+        static void Main(string[] args)
+        {
+            string test = "Success";
+            
+            Console.WriteLine(DuplicateEncode(test) == ")())())");
+            Console.ReadKey();
+        }
+    }
+}
