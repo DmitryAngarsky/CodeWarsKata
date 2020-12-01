51c8760baac08ae68db2a2ab3e7dac52c7ca405c
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-12-01 22:04:05 +0400

    Add Special_Numbers_5

---
+++ Special_Numbers_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Special_Numbers_5
+{
+    class Program
+    {
+        public static string SpecialNumber(int number)
+        {
+            return $"{number}".All(n => n - '0' <= 5) 
+                ? "Special!!" 
+                : "NOT!!";
+        }
+        
+        static void Main(string[] args)
+        {
+            const int test = 513;
+            
+            Console.WriteLine(SpecialNumber(test) == "Special!!");
+            Console.ReadKey();
+        }
+    }
+}
