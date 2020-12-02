ad5ead7eb840512b604a57428e28865882fc436f
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-12-02 19:52:17 +0400

    Add ComplementaryDNA

---
+++ ComplementaryDNA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ComplementaryDNA
+{
+    class Program
+    {
+        public static string MakeComplement(string dna)
+        {
+            return string.Concat(dna.Select(c => c switch
+            {
+                'A' => 'T',
+                'T' => 'A',
+                'G' => 'C',
+                'C' => 'G',
+                _ => c
+            }));
+            return string.Concat(dna.Select(c => "AGCT"["TCGA".IndexOf(c)]));
+        }
+        
+        static void Main(string[] args)
+        {
+            string test = "TAACG";
+            
+            Console.WriteLine(MakeComplement(test));
+            Console.ReadKey();
+        }
+    }
+}
