d309d66dbf193b818445d7440e6b8fc82ba3ef90
DmitryAngarsky <femto8032@gmail.com>
2021-01-19 10:30:57 +0400

    Add StringBreakers

---
+++ StringBreakers/StringBreakers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace StringBreakers
+{
+    class Program
+    {
+        private static string StringBreakers(int n, string str)
+        {
+            string emptyStr = str.Replace(" ", "");
+            int[] indexesToSplit = Enumerable
+                .Range(0, emptyStr.Length)
+                .Where(w => w % n == 0)
+                .ToArray();
+
+            string first = string.Concat(
+                indexesToSplit
+                    .Take(indexesToSplit.Length - 1)
+                    .Select(i => emptyStr[i..(i + n)] + "\n"));
+            
+            string second = emptyStr.Substring(indexesToSplit.Last());
+            
+            return first + second;
+            
+            //return string.Concat(str.Replace(" ", "").Select((x,i) => i != 0 && i % n == 0 ? "\n" + x : x.ToString()));
+        }
+        
+        static void Main(string[] args)
+        {
+            const string str = "This is an example string";
+            const int count = 5;
+            
+            Console.WriteLine(StringBreakers(count, str));
+        }
+    }
+}
